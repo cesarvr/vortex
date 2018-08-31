64350f9b937c83f755edba4455911b6646adb3ca
Cesar Valdez <cvaldezrx@gmail.com>
2018-08-31 12:09:33 +0100

    - updating vortex  time: 12:09:33 date: 31-08-2018

--- update.sh
+++ update.sh
@@ -1,3 +1,13 @@
+npm run build
+
+branch=$(git branch | grep \* | cut -d ' ' -f2)
+dest_folder=../demos-vortex/static
+
+folder=$dest_folder/$branch
+
+mkdir -p $folder 
+cp -r index.html dist $folder  
+
 git add .
 git commit -m " - updating vortex  time: $(date +"%T") date: $(date +"%d-%m-%Y")"
-git push origin master
+git push origin $branch
